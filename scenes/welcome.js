dd7d5b5ccc518a8073499d223f0fe961d4ea09db
Humberto Filho <HumbertoFilhoCastro@gmail.com>
2025-03-06 08:55:45 -0300

    Loading some assets + Main js files

---
+++ scenes/welcome.js
@@ -0,0 +1,21 @@
+class Welcome extends Phaser.Scene{
+
+
+    constructor(){
+        super({
+            key: "Welcome",
+            background: "#000", // Configuração da cor de fundo da cena
+        })
+    }
+
+    preload(){
+        this.load.image('assetss/welcome.png');
+    }
+
+    create(){
+    }
+
+    preload(){
+
+    }
+}
